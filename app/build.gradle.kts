99ffb82c14d2c58b6010df46342a6ce446b84874
KIRISHIKI Yudai <yudai.kirishiki@protonmail.com>
2026-07-15 16:03:57 +0900

    set version v0.1.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "jp.unknowntech.mobilemelon"
         minSdk = 24
         targetSdk = 36
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "0.1.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
